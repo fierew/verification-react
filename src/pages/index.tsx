713fc09e881664e7742a5da0e81df90d8ce45bf6
xuyang <xuyang_5@163.com>
2020-09-13 20:48:58 +0800

    123

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,16 +5,13 @@ export default () => {
   const { initialState, loading, error, refresh, setInitialState } = useModel(
     '@@initialState',
   );
-  console.log(initialState);
 
   const menu = initialState?.menu ?? [];
 
-  let homeUrl = '/';
-  if (menu.length > 0) {
-    homeUrl = menu[0].path;
-  }
   useEffect(() => {
-    history.push(homeUrl);
+    if (menu.length > 0) {
+      history.push(menu[0].path);
+    }
   }, []);
 
   return <></>;
